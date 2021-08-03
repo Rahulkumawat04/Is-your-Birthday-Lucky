c77d9ec716210154dd06257f32b0626c02e992c3
Rahul Kumawat <mailrahul2004@gmail.com>
2021-08-03 16:10:34 +0530

    complete

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const dateOfBirth = document.querySelector("#date-of-birth");
-const luckyNumber = document.querySelector("#lucky-number");
-const checkNumberButton = document.querySelector("#check-number");
-const outputBox = document.querySelector("#output-box")
+var dateOfBirth = document.querySelector("#date-of-birth");
+var luckyNumber = document.querySelector("#lucky-number");
+var checkNumberButton = document.querySelector("#check-number");
+var outputBox = document.querySelector("#output-box")
 
 function compareValue(sum, luckyNumber){
-    if(sum%luckyNumber===0){
-        console.log("Your birthday is lucky!!");
+    if(sum%luckyNumber === 0){
+        outputBox.innerHTML = "🎈Yayy!!! Your birthday is lucky!!🎉🥳";
     }
     else{
-        console.log("Your birthday is NOT lucky!!");
+        outputBox.innerHTML = "Oops!! Sorry,looks like your birthday is not that lucky!!😟😪";
 
     }
 }
 
 function checkBirthdateIsLucky(){
-    var date = dateOfBirth.value;
+    var dob = dateOfBirth.value;
     var sum = calculateSum(dob);
-    compareValue(sum, luckyNumber.value)
+    compareValue(sum, luckyNumber.value);
 }
 
 function calculateSum(dob){
     dob = dob.replaceAll("-","");
-    let sum = "0";
+    let sum = 0;
     for(let index=0; index < dob.length; index++){
         sum = sum + Number(dob.charAt(index));
-    }
+    }   
     return sum;
 }
 
-checkNumberButton.addEventListener('click', calculateSum);
+checkNumberButton.addEventListener('click', checkBirthdateIsLucky);
